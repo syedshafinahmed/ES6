db21f9a230bba955e5f4223c49582c0206182b58
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-20 21:58:05 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { add, diff, mult, divide as div } from './utils/math/math'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const sum = add(2, 5);
+  const subs = diff(10, 7);
+  const goonfol = mult(5 * 7);
+  const divide = 10 / 5;
+  const something = div(21, 3);
+  console.log(sum, subs, goonfol, divide, something);
 
   return (
     <>
---
+++ src/utils/math/math.js
@@ -0,0 +1,6 @@
+const add = (a, b) => a + b;
+const diff = (a, b) => a - b;
+const multiplyTwoNUmbers = (a, b) => a * b;
+const divide = (a, b) => a/ b;
+
+export {add, diff, multiplyTwoNUmbers as mult, divide}
